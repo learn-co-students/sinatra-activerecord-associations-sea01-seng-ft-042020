6d27be396bb60ddef8d7dca505ac36d97bb0d49f
Trevor <tlow94@uw.edu>
2020-05-13 10:51:00 -0700

    Done.

--- models/cat.rb
+++ models/cat.rb
@@ -1,3 +1,10 @@
 class Cat < ActiveRecord::Base
   belongs_to :owner
+  def change
+    create_table :cats do |t|
+      t.string :name
+      t.integer :age
+      t.string :breed
+    end
+  end
 end
